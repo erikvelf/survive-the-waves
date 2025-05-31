5c0e02d110dcdbf96db0e6c2fd0c385cd9d36799
Erik Velf <velf.erik@protonmail.com>
2025-05-31 12:59:21 +0200

    Unito il codice per cura

--- components/generic_enemy.gd
+++ components/generic_enemy.gd
@@ -1,7 +1,10 @@
 extends CharacterBody3D
 
 @onready var hurt_sound: AudioStreamPlayer = $HurtSound
+@onready var player: CharacterBody3D = get_tree().get_nodes_in_group("Player")[0]
 
 func _on_hurtbox_received_damage(damage: int) -> void:
 	hurt_sound.play()
-	
+
+func _physics_process(delta: float) -> void:
+	look_at(Vector3(player.position.x, position.y, player.position.z))
