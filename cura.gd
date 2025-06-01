f97a69283aca1581a108b297933e1a0f6ea98e7a
Erik Velf <velf.erik@protonmail.com>
2025-06-01 08:07:59 +0200

    Add model and healing ability to Cura

--- cura.gd
+++ cura.gd
@@ -1,5 +1,6 @@
 extends Node3D
 
+@export var healing: int = 30
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,4 +13,8 @@ func _process(delta: float) -> void:
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	queue_free()
+	if body is CharacterBody3D:
+		var health: Health = body.get_node("Health")
+		health.set_health(health.health + healing)
+		queue_free()
+			
