4e3941d4e58ead7ae4d86d0ce181d472ccafcd05
Erik Velf <velf.erik@protonmail.com>
2025-06-02 09:05:44 +0200

    Add healing sound to cura.tscn

--- cura.gd
+++ cura.gd
@@ -2,19 +2,19 @@ extends Node3D
 
 @export var healing: int = 30
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	rotate(Vector3.UP, 0.01) # rotate the boost
-
+	rotate(Vector3.UP, 1 * delta)
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	if body is CharacterBody3D:
+	if body is CharacterBody3D and body.is_in_group("Player"):
 		var health: Health = body.get_node("Health")
 		health.set_health(health.health + healing)
-		queue_free()
-			
+		var audio_player = $AudioStreamPlayer
+		
+		audio_player.play()
+		audio_player.connect("finished", Callable(self, "_on_sound_finished"))  # Wait for sound to finish
+		$Area3D.monitoring = false  # Prevent double healing
+		$Model.hide()
+
+func _on_sound_finished():
+	queue_free()
